591fd8ed5a1a66963fb05956b5fb4e9fde114f62
Nick Pack <nick@nickpack.com>
2012-05-22 22:45:28 +0100

    Initial work on the formatting

--- basscouts/settings.py
+++ basscouts/settings.py
@@ -13,12 +13,10 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'scouts.sqlite',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'scoutsdb',
+        'USER': 'basscouts',
+        'PASSWORD': 'nykrttsx'
     }
 }
 
@@ -116,7 +114,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'scouts',
-    'south',
+    #'south',
     'sorl.thumbnail',
     'gunicorn',
     'bootstrapform',
